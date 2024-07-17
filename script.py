233dba844c7b2a36a8424280cb995f50b9f3ed0e
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-07-17 16:30:40 -0400

    Update import syntax

--- script.py
+++ script.py
@@ -7,12 +7,12 @@ Planning:
      - (can I afford x at this time? how much money will I need on y date to afford my bills before I get paid? etc.).
  - Design visual layout.
 """
-import datetime
+from datetime import date
 
 class Balance:
     def __init__(self, amount=0):
         self.amount: float = amount
-        self.date = datetime.date.today()
+        self.date = date.today()
 
     def add(self, amount):
         self.amount += amount
@@ -28,13 +28,13 @@ class Bill:
     def __init__(self, name, amount, due_date, is_paid=False):
         self.name: str = name
         self.amount: float = amount
-        self.due_date:datetime.date = due_date
+        self.due_date: date = due_date
         self.is_paid: bool = is_paid
         self.is_late: bool = False
 
     def check_due_date(self):
         print(f"'{self.name}' is due on {self.due_date}")
-        if self.due_date < datetime.date.today():
+        if self.due_date < date.today():
             self.is_late = True
             print(f"'{self.name}' is late!")
 
@@ -46,15 +46,15 @@ class Bill:
 
     def check_is_late(self):
         if self.is_late:
-            days_past_due = datetime.date.today() - self.due_date  
+            days_past_due = date.today() - self.due_date  
             print(f"'{self.name}' is {days_past_due.days} days past due!")
         else:
-            days_until_due = self.due_date - datetime.date.today() 
+            days_until_due = self.due_date - date.today() 
             print(f"You still have {days_until_due.days} days until '{self.name}' is due.")
 
 
 account = Balance(1000)
-bill = Bill("Example", 250, datetime.date(2024, 7, 30))
+bill = Bill("Example", 250, date(2024, 7, 30))
 bill.check_due_date()
 bill.check_is_paid()
 bill.check_is_late()
